a4e249cc30a7b8b44978f8dc8f4906f5460d62fe
ji4814yx <ji4814yx@go.minnstate.edu>
2019-12-04 11:36:31 -0600

    Initial commit

---
+++ Random_Taco_Cookbook.py
@@ -0,0 +1,37 @@
+import docx # import this library to create and update a word document
+from docx.shared import Inches
+from docx.enum.text import WD_BREAK
+from docx.enum.text import WD_BREAK # source: stack overflow
+document = docx.Document() # create a document
+
+document.add_heading("Random Taco Cookbook", 0) # add the heading Random Taco Cookbook
+
+document.add_picture('Modified_Tai_Taco.jpg', width=Inches(6), height=Inches(4)) # add the resized taco image
+
+document.add_paragraph("Photo by Tai's Captures on Unsplash") # write the name of the image author
+
+document.add_paragraph()
+
+document.add_paragraph("Source: https://taco-1150.herokuapp.com/random/?full_taco=true") # to write the URL text
+document.add_paragraph()
+document.add_paragraph()
+document.add_paragraph()
+document.add_paragraph()
+document.add_paragraph()
+document.add_paragraph()
+
+document.add_paragraph("Adade Gbadoe", "Heading 3") # to write my name with Heading 3 style
+
+for paragraph in document.paragraphs:
+    if 'Adade Gbadoe' in paragraph.text:
+        run = paragraph.add_run()
+        run.add_break(WD_BREAK.PAGE)
+#document.paragraphs[0].runs[0].add_break(docx.text.WD_BREAK.PAGE)
+
+# document.paragraphs[0].runs[0].add_break()
+document.save('First_Page.docx')
+
+
+
+
+
